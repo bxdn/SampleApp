ca7f39774edaf4147f5c5a30f88da711700e8b6c
bxdn <sean.smolinski@gmail.com>
2018-10-11 19:21:53 -0400

    Simple Edit (Pre Test Implementation)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,17 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
   def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
